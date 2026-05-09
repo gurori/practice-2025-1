94fad1ef2d9327ab4fcb981ec8116a4646ec0c2b
gurori <nazarovslava124@gmail.com>
2026-05-09 19:07:22 +0300

    Update footer

--- site/src/components/Layout/Footer.tsx
+++ site/src/components/Layout/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
-    return (
-        <footer className="container py-16">
-            2026, Москва
-        </footer>
-    )
+  return (
+    <footer className="container py-16">
+      <div className="mt-10 border-t border-slate-200 pt-6">
+        <p className="text-sm text-slate-500">
+          © 2026 Живой стилистический словарь русского языка
+        </p>
+      </div>
+    </footer>
+  );
 }
